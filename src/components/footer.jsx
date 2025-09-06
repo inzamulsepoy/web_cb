6999fdd4cfb0e78d6acfd1582e5682c60fdc0fe8
root <root@windows.user>
2025-09-06 23:37:31 +0530

    changes

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-gray-400 text-center py-4">
+      <p>© {new Date().getFullYear()} Pentestpro. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+// Home.jsx
+import About from "./about";
+import Contact from "./contact";  
+
+export default function Home() {
+  return (
+    <div>
+      
+      {/* About Section */}
+      <About />
+
+      {/* Contact Section */}
+      <Contact />
+    </div>
+  );
+}
