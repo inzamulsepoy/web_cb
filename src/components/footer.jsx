ce68e40d37a5cdfe97cd211d47992ad4dd954438
root <root@windows.user>
2025-09-06 23:53:07 +0530

    changes

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-gray-800 text-gray-400 text-center py-4">
-      <p>© {new Date().getFullYear()} Pentestpro. All rights reserved.</p>
-    </footer>
-  )
-}
-
-export default Footer
