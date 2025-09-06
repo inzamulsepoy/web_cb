492701bdf42021a94d3f97efc496ff85d6bb431b
root <root@windows.user>
2025-09-06 23:29:53 +0530

    changes

--- src/app/page.js
+++
@@ -1,9 +0,0 @@
-import HeroSection from "@/components/HeroSection";
-
-export default function Home() {
-  return (
-    <main className=" min-h-screen flex flex-col items-center">
-      <HeroSection></HeroSection>
-    </main>
-  );
-}
